5d2bf27efac6bff30d7de581dcd83f999e151241
Helen Evans <helenijevans@gmail.com>
2020-03-11 21:11:02 +0000

    added more test cases

--- trainAndTest.py
+++ trainAndTest.py
@@ -61,7 +61,7 @@ def main():
 
     kNearest.train(npaFlattenedImages, cv2.ml.ROW_SAMPLE, npaClassifications)
 
-    imgTestingNumbers = cv2.imread("test4.png")          # read in testing numbers image
+    imgTestingNumbers = cv2.imread("test5.png")          # read in testing numbers image
 
     if imgTestingNumbers is None:                           # if image was not read successfully
         print "error: image not read from file \n\n"        # print error message to std out
